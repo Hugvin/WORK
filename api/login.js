8fc88997561f1572b17ac871cb19ed8153809b56
Hugvin <156345324+Hugvin@users.noreply.github.com>
2024-08-17 14:12:56 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,1 +1,9 @@
 
+export default (req, res) => {
+  const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
+  const REDIRECT_URI = `${process.env.BASE_URL}/api/callback`;
+  const SCOPE = 'identify guilds';
+  
+  const discordAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=${SCOPE}`;
+  res.redirect(discordAuthUrl);
+};
