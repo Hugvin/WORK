84dff0a9d65832073d41f44a7a65f6c8c0b0fd8a
Hugvin <156345324+Hugvin@users.noreply.github.com>
2024-08-27 15:12:42 +0200

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,1 +1,10 @@
 
+// api/ping.js
+
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).send('Ping received');
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+}
